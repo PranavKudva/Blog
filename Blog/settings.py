ad3a99db47695a64bcf44bec1c9bebb658078553
Pranav Kudva Vishwanath <pranavkv@thoughtworks.com>
2013-12-30 21:00:49 +0530

    [Chethan, Pranav] Done with rendering CSS and HTML with bootstrap changed to appear as sketch UI.

--- Blog/settings.py
+++ Blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(__file__)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -35,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,7 +77,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# https://docs.djangoproject.com/en/1.6/howto/bootstrap-files/
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
-STATIC_ROOT = '~/Blog/static'
-STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'bootstrap/'))
--- Blog/urls.py
+++ Blog/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
 from Blog import settings
+from django.conf.urls import patterns, url
 
 # from django.contrib import admin
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    url(r'home/', TemplateView.as_view(template_name='index.html'))
-)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = patterns('', url(r'home/', TemplateView.as_view(template_name='createBlog.html')),
+    (r'bootstrap/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT})
+)
